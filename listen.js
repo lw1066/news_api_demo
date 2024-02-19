1dccf3ade8426f333f96ff43111909701fe74cb6
lw1066 <lewis_webster@hotmail.com>
2024-02-19 12:27:01 +0000

    get /api/topics endpoint added with tests

---
+++ listen.js
@@ -0,0 +1,11 @@
+function listen(){
+    app.listen(9090, (err) => {
+        if(err){
+            console.log(err)
+        } else {
+            console.log("listening on 9090")
+        }
+    })
+}
+
+module.exports = listen;
