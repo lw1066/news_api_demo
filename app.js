9751801c04c74e1068392de2fb67209d8a8c020d
lw1066 <lewis_webster@hotmail.com>
2024-02-20 13:13:19 +0000

    added POST/api/articles/:article_id/comments

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { getApiMapController } = require('./controllers/get-api-map.controller');
 const { getArticleByIdController } = require('./controllers/get-article-by-id.controller');
 const { getAllArticlesController } = require('./controllers/get-all-articles.controller');
 const { getAllCommentsForArticleController } = require('./controllers/get-all-comments-for-article.controller');
+const { postNewCommentController } = require('./controllers/post-new-comment.controller');
 
 const app= express();
 
@@ -16,6 +17,7 @@ app.get('/api/topics', getAllTopics);
 app.get('/api/articles', getAllArticlesController);
 app.get('/api/articles/:article_id', getArticleByIdController);
 app.get('/api/articles/:article_id/comments', getAllCommentsForArticleController);
+app.post('/api/articles/:article_id/comments', postNewCommentController);
 
 app.use((req, res, next) => {
     const err = new Error(`Page not found - ${req.originalUrl}`);
@@ -23,10 +25,12 @@ app.use((req, res, next) => {
     next(err);
 });
 
-
 app.use((err, req, res, next) => {
     if(err.code === '22P02') {
         res.status(400).send({msg:'bad request - invalid id'});
+    }
+    if(err.code === '23503') {
+        res.status(400).send({msg: `bad request - ${err.detail}`})
     }
     next(err);
 })
