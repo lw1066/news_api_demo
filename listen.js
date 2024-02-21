9e0c1d56dd01991bbc9b312468eda7adf63f79e3
lw1066 <lewis_webster@hotmail.com>
2024-02-21 11:56:00 +0000

    hosting config update

--- listen.js
+++ listen.js
@@ -3,10 +3,4 @@ const app = require('./app.js');
 
 const { PORT = 9090 } = process.env;
 
-    app.listen(PORT, (err) => {
-        if(err){
-            console.log(err)
-        } else {
-            console.log("listening on 9090")
-        }
-    })
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
