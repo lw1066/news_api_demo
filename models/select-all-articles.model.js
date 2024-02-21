0bafbc6240bd9aee30ff329c07b77eb06b57d721
lw1066 <lewis_webster@hotmail.com>
2024-02-21 09:26:19 +0000

    added comment_count to data servered on api/articles/:article_id - also edited for succinctness  sql statements for select-all-articles and select-article-by-id models

--- models/select-all-articles.model.js
+++ models/select-all-articles.model.js
@@ -2,26 +2,20 @@ const db = require('../db/connection');
 
 exports.selectAllArticles = async () => {
     const articles = await db.query(
-        `SELECT articles.article_id, 
-            articles.title, 
-            articles.topic, 
-            articles.author, 
-            articles.created_at, 
-            articles.votes, 
-            articles.article_img_url, 
-            COUNT(comments.article_id) AS comment_count 
-        FROM articles 
-        LEFT JOIN comments 
-        ON articles.article_id = comments.article_id
+        `SELECT a.article_id, 
+            a.title, 
+            a.topic, 
+            a.author, 
+            a.created_at, 
+            a.votes, 
+            a.article_img_url, 
+            COUNT(c.article_id)::INTEGER AS comment_count 
+        FROM articles a
+        LEFT JOIN comments c
+        ON a.article_id = c.article_id
         GROUP BY 
-            articles.article_id, 
-            articles.title, 
-            articles.topic, 
-            articles.author, 
-            articles.created_at, 
-            articles.votes, 
-            articles.article_img_url
-        ORDER BY articles.created_at DESC;`
+            a.article_id   
+        ORDER BY a.created_at DESC;`
     );
     return articles.rows;
 };
