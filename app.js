a7306a7ce42670a433a271d63b42550f4149fe4a
lw1066 <lewis_webster@hotmail.com>
2024-03-11 10:34:05 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
-const express = require('express');
-const app= express();
-const apiRouter = require('./routes/api-router');
+const express = require("express");
+const app = express();
+const apiRouter = require("./routes/api-router");
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 
-app.use('/', apiRouter);
+app.use("/", apiRouter);
 
 app.use((req, res, next) => {
-    const err = new Error(`Page not found - ${req.originalUrl}`);
-    err.statusCode = 404;
-    next(err);
+  const err = new Error(`Page not found - ${req.originalUrl}`);
+  err.statusCode = 404;
+  next(err);
 });
 
 app.use((err, req, res, next) => {
-    if(err.code === '22P02') {
-        res.status(400).send({msg: err.msg ||'bad request - invalid id'});
-    }
-    if(err.code === '23503') {
-        console.log('err in app-----------', err)
-        res.status(404).send({msg: err.msg || `provided details not found`, errDetails: `PSQL error code - ${err.code}, details -${err.detail}`})
-    }
-    if(err.code === '23502') {
-        res.status(400).send({msg: `bad request - missing information`})
-    }
-    next(err);
-})
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: err.msg || "bad request - invalid id" });
+  }
+  if (err.code === "23503") {
+    console.log("err in app-----------", err);
+    res
+      .status(404)
+      .send({
+        msg: err.msg || `provided details not found`,
+        errDetails: `PSQL error code - ${err.code}, details -${err.detail}`,
+      });
+  }
+  if (err.code === "23502") {
+    res.status(400).send({ msg: `bad request - missing information` });
+  }
+  next(err);
+});
 
 app.use((err, req, res, next) => {
-    res.status(err.statusCode || 500).send({msg: err.message});
+  res.status(err.statusCode || 500).send({ msg: err.message });
 });
 
 module.exports = app;
